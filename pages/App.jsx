c11aa00ef2859a6f350d9576c68313a37a65c8f0
kannan <kannan@cloudrevelinnovation.com>
2024-04-15 18:11:55 +0530

    Done Deign Cahnge

--- pages/App.jsx
+++ pages/App.jsx
@@ -6,17 +6,16 @@ import {  Card, Col, Row} from 'antd';
 function App() {
   return (
     <>
-    <Row style={{ paddingTop: 120 }}>
-      <Col span={12} offset={6}>
-        <Card title="TODO LIST" style={{ width: 700 }}>
-          <div className="App">
-            <TodoAdd />
-
-            <TodoList />
-          </div>
-        </Card>
-      </Col>
-    </Row>
+      <Row justify="center" align="middle" style={{ paddingTop: 120 }}>
+        <Col span={12}>
+          <Card title="TODO LIST">
+            <div className="App">
+              <TodoAdd />
+              <TodoList />
+            </div>
+          </Card>
+        </Col>
+      </Row>;
 
     </>
   )
